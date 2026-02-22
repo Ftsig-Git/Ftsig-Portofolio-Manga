4ffe50b354a94895b160e396e767ca4f467ff647
Ftsig-Git <cosmic501245@gmail.com>
2026-02-22 22:36:25 +0700

    Create manga library page

---
+++ pages/manga.tsx
@@ -0,0 +1,14 @@
+// manga.tsx
+
+import React from 'react';
+
+const MangaLibrary: React.FC = () => {
+    return (
+        <div>
+            <h1>Manga Library</h1>
+            {/* Add manga collection component here */}
+        </div>
+    );
+};
+
+export default MangaLibrary;
